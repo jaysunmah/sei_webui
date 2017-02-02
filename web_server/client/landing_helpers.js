d67e51436402cd36bbd490a8d9e4c2f7447a3560
Jason Ma <jaysunmah@gmail.com>
2017-02-02 11:24:09 -0500

    added new file

---
+++ web_server/client/landing_helpers.js
@@ -0,0 +1,80 @@
+var ros;
+var listener;
+var test;
+var t;
+
+calibrationEventHandler = function(id) {
+  if (id == "calibrateLeft") {
+    if (Session.get('mouseSelect') == 'left') {
+      Session.set('mouseSelect', 'no_select');
+      Session.set('openedSetRobotInitPosAccordion', false);
+    } else {
+      Session.set('mouseSelect', 'left');
+    }
+  } else if (id == "calibrateRight") {
+    if (Session.get('mouseSelect') == 'right') {
+      Session.set('mouseSelect', 'no_select');
+      Session.set('openedSetRobotInitPosAccordion', false);
+    } else {
+      Session.set('mouseSelect', 'right');
+    }
+  } else if (id == "calibrateTop") {
+    if (Session.get('mouseSelect') == 'top') {
+      Session.set('mouseSelect', 'no_select');
+      Session.set('openedSetRobotInitPosAccordion', false);
+    } else {
+      Session.set('mouseSelect', 'top');
+    }
+  } else if (id == "calibrateBottom") {
+    if (Session.get('mouseSelect') == 'bottom') {
+      Session.set('mouseSelect', 'no_select');
+      Session.set('openedSetRobotInitPosAccordion', false);
+    } else {
+      Session.set('mouseSelect', 'bottom');
+    }
+  }
+}
+
+rosConnect = function(ip) {
+  console.log("Connecting to ip address: " + ip);
+  ros = new ROSLIB.Ros({
+      url : ip
+  });
+  ros.on('connection',function() {
+    console.log('Connected to websocket server.');
+    Session.set('rosConnectionStatus', true);
+  });
+
+  ros.on('error',function() {
+    console.log('Error: ',error);
+  });
+  ros.on('close',function() {
+    console.log('Closed');
+  });
+
+  listener = new ROSLIB.Topic({
+    ros: ros,
+    name : '/helloBridge',
+    messageType : 'std_msgs/String'
+  });
+
+  listener.subscribe(function(message) {
+    console.log('Message: ' + message.data);
+    Session.set('runtimeStatus', message.data);
+  });
+
+  test = new ROSLIB.Topic({
+    ros: ros,
+    name: '/jason',
+    messageType: 'std_msgs/String'
+  });
+}
+
+rosSendMessage = function(data) {
+  t = new ROSLIB.Message({
+    data: data,
+  });
+
+  test.publish(t);
+  console.log('sent!');
+}
